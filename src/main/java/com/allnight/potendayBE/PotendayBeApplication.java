d0966b9f12451a45517d351a3d29e09ac208326a
rudalsss <linda284@naver.com>
2025-08-05 03:15:37 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/allnight/potendayBE/PotendayBeApplication.java
@@ -0,0 +1,13 @@
+package com.allnight.potendayBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotendayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotendayBeApplication.class, args);
+	}
+
+}
