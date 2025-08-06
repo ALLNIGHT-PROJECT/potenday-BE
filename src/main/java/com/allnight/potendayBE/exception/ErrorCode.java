37d08c9ee2ef001e5fb9467cc1b52ae1ea3f131e
rudalsss <linda284@naver.com>
2025-08-07 00:17:05 +0900

    [feat] security 기본설정

--- src/main/java/com/allnight/potendayBE/exception/ErrorCode.java
+++ src/main/java/com/allnight/potendayBE/exception/ErrorCode.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근권한이 없습니다.");
+    // Spring security 인증 & 인가 실패
+    ACCESS_TOKEN_MISSING(HttpStatus.UNAUTHORIZED, "AccessToken이 요청에 포함되어있지 않습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근권한이 없습니다.")
+    ;
 
-    private final HttpStatus httpStatus;
+    private final HttpStatus status;
     private final String message;
 }
